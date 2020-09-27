c190a9f6c0e9cb2a712d1fde518873f9ff1ff9c0
sarah-blanchard <71983673+sarah-blanchard@users.noreply.github.com>
2020-09-27 14:44:01 -0500

    Update peerReviewErrors.py
    adjusted line breaks

--- peerReviewErrors.py
+++ peerReviewErrors.py
@@ -1,18 +1,14 @@
 # This is a header for the application
 # You should read this header and insert your name and your date below as part of the peer review
 # This is a typical part of any program
-# Author: <author>
-# Creation Date: <date>
+# Author: <Blanchard>
+# Creation Date: <27 September 2020>
 # Below is a simple program with 10 issues (some are syntax errors and some are logic errors.  You need to identify the issues and correct them.
 
 import random
 import time
 
 def displayIntro():
-	print('''You are in a land full of dragons. In front of you,
-	you see two caves. In one cave, the dragon is friendly
-	and will share his treasure with you. The other dragon
-	is greedy and hungry, and will eat you on sight.''')
 	print()
 
 def chooseCave():
@@ -33,6 +29,7 @@ def checkCave(chosenCave):
 	print()
 	#sleep for 2 seconds
 	time.sleep(2)
+	
 	friendlyCave = random.randint(1, 2)
 
 	if chosenCave == str(friendlyCave):
